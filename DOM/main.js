663fc522129af998eccf551dca3d71eb138310b1
mehtashish <mehta.mtj@gmail.com>
2024-11-13 23:31:04 +0530

    intro to DOM

---
+++ DOM/main.js
@@ -0,0 +1,16 @@
+// console.log(document);
+
+const h1 = document.getElementById("head");
+console.log(h1);
+
+const pwithclass = document.getElementsByClassName("cl2");
+console.log(pwithclass);
+
+const allP = document.getElementsByTagName("p");
+console.log(allP);
+
+const eleByQuery = document.querySelector(".cl2");
+console.log(eleByQuery);
+
+const allByQuery = document.querySelectorAll(".cl2");
+console.log(allByQuery);
