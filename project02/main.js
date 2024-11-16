ae8c74057e42584f6b6b4fc766c05cc7320efbf3
mehtashish <mehta.mtj@gmail.com>
2024-11-16 17:41:57 +0530

    movie recommendation project

---
+++ project02/main.js
@@ -0,0 +1,42 @@
+const form = document.getElementById("movieForm");
+const movieContainer = document.getElementById("container");
+
+const createMovieElement = (movies) => {
+    movieContainer.innerHTML = "";
+
+    for (let movie of movies) {
+        const div = document.createElement("div");
+        div.setAttribute("class", "singleMovie");
+
+        const banner = document.createElement("img");
+        banner.className = "banner";
+
+        banner.setAttribute("src", movie.show.image.original);
+        let summary = movie.show.summary.slice(0, 100);
+        if(movie.show.summary.length > 100) {
+            summary += "...";
+        }
+        
+        div.innerHTML = `<h2>${movie.show.name}</h2> <p>${movie.show.rating.average}</p> ${summary}`;
+        div.prepend(banner);
+        movieContainer.append(div);
+    }
+}
+
+const getMovies = async (search) => {
+    let res = await fetch(`http://api.tvmaze.com/search/shows?q=${search}`);
+    let data = await res.json();
+    createMovieElement(data);
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let search = e.target.children[0].value;
+    e.target.children[0].value = "";
+    getMovies(search);
+})
+
+
+// console.log(form.parentElement);
+// console.log(form.nextElementSibling);
+// console.log(form.previousElementSibling);
