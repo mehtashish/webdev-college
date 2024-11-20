55d12e2ebeac7726cd874a3ef58665021fc77f12
mehtashish <mehta.mtj@gmail.com>
2024-11-20 21:26:04 +0530

    intro to react library

---
+++ React/React-project/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import "./Navbar.css"
+
+const Navbar = () => {
+    return (
+        <div className="w-screen text-2xl bg-black text-white fixed top-0 p-5">
+            This is Navbar
+        </div>
+    )
+}
+
+export default Navbar;
