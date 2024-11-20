1d5aafa79df5de37069115aeb095ca4ee24c3b2a
mehtashish <mehta.mtj@gmail.com>
2024-11-20 11:02:16 +0530

    viewing innerHTML content

--- DOM/main.js
+++ DOM/main.js
@@ -1,16 +1,27 @@
 // console.log(document);
 
 const h1 = document.getElementById("head");
-console.log(h1);
+// console.log(h1);
 
-const pwithclass = document.getElementsByClassName("cl2");
-console.log(pwithclass);
+// const pwithclass = document.getElementsByClassName("cl2");
+// console.log(pwithclass);
 
-const allP = document.getElementsByTagName("p");
-console.log(allP);
+// const allP = document.getElementsByTagName("p");
+// console.log(allP);
 
-const eleByQuery = document.querySelector(".cl2");
-console.log(eleByQuery);
+// const eleByQuery = document.querySelector(".cl2");
+// console.log(eleByQuery);
 
-const allByQuery = document.querySelectorAll(".cl2");
-console.log(allByQuery);
+// const allByQuery = document.querySelectorAll(".cl2");
+// console.log(allByQuery);
+
+
+// attributes and methods, accessors
+
+console.log(h1.innerText);
+console.log(h1.innerHTML);
+console.log(h1.textContent);
+console.log(h1.id);
+console.log(h1.className);
+console.log(h1.classList);
+h1.classList.add("cl3");
